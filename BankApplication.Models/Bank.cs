68d9dd4645a47d72a00912988b1ed7b84c8543ef
Grahitha <grahitha.eda@gmail.com>
2021-10-04 18:14:48 +0530

    Bank-Application

---
+++ BankApplication.Models/Bank.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+//using Newtonsoft.Json;
+
+namespace BankApplication1.Oops
+{
+    public class Bank
+    {
+
+        public string name { get; set; }
+        public int id { get; set; }
+        public string address { get; set; }
+        public string branch { get; set; }
+
+        public List<BankAccount> BankAccounts = new List<BankAccount>();
+        public static List<string> transactions = new List<string>();
+        public Bank(string name, int id, string address, string branch)
+        {
+            this.name = name;
+            this.id = id;
+            this.address = address;
+            this.branch = branch;
+            //string output = File.ReadAllText(@"E:\\account.json");
+            //BankAccounts= JsonConvert.DeserializeObject<List<BankAccount>>(output);
+            Console.WriteLine("Welcome to India Bank!");
+
+        }
+        public BankAccount login(string accountHolder, string pass)
+        {
+            BankAccount user = null;
+            foreach (BankAccount account in BankAccounts)
+            {
+                if (account.name == accountHolder & account.password == pass)
+                {
+                    user = account;
+                    Bank.transactions.Add(accountHolder + " account logged in!");
+                }
+
+            }
+            return user;
+        }
+        public BankAccount checkAccount(string accountHolder)
+        {
+            BankAccount user = null;
+            foreach (BankAccount account in BankAccounts)
+            {
+                if (account.name == accountHolder)
+                    user = account;
+            }
+            return user;
+        }
+
+    }
+}
---
+++ BankApplication.Services/BankManager.cs
@@ -0,0 +1,29 @@
+﻿using BankApplication1.Oops;
+using System;
+using System.Linq;
+
+namespace IndiaBank.Oops.services
+{
+    public class BankManager
+    {
+        public Bank bank = new Bank("IndiaBank", 2, "Kollipara", "SBI");
+
+        public void createaccount(string accountHolder, int number, string pass)
+        {
+
+            if (string.IsNullOrEmpty(accountHolder))
+                throw new Exception("Name is not valid!");
+            if (bank.BankAccounts.Count != 0 & bank.BankAccounts.Any(p => p.name == accountHolder) == true)
+                throw new Exception("Account already exists!");
+
+            BankAccount ba = new BankAccount();
+            ba.name = accountHolder;
+            ba.phoneNumber = number;
+            ba.id = bank.BankAccounts.Count + 1;
+            ba.bankId = bank.id;
+            ba.password = pass;
+            bank.BankAccounts.Add(ba);
+            Bank.transactions.Add(accountHolder + " account created successfully!");
+        }
+    }
+}
